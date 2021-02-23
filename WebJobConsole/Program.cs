8a02d1e33648edfbf484cb52b9e430f2d86ae006
Joe McLeary <jmcleary@ducks.org>
2021-02-23 15:41:20 -0600

    add new console output

--- WebJobConsole/Program.cs
+++ WebJobConsole/Program.cs
@@ -11,6 +11,8 @@ namespace WebJobConsole
         static void Main(string[] args)
         {
             Console.WriteLine("hello world webjob test");
+
+            Console.WriteLine("editing the file");
         }
     }
 }
